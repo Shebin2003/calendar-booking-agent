d1df8413881cee5d9a1fb4f74c916e36414854ee
Shebin2003 <shebinshamsuddeen2003@gmail.com>
2025-07-02 17:10:13 +0530

    second

--- backend/main.py
+++ backend/main.py
@@ -5,9 +5,10 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], 
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 @app.post("/chat")
